4f8b8e4f904b5037ef7ac83144e1a2a85c568d98
Doomturtles <jonny.collins111@gmail.com>
2024-11-21 21:15:14 +0000

    added GlobalSettingsProfile, a way to keep track of the server host, screen size, and other settings in future

---
+++ src/GlobalSettings.cpp
@@ -0,0 +1,6 @@
+#include "GlobalSettings.h"
+GlobalSettingsProfile::GlobalSettingsProfile() {
+	host = nullptr;
+	screenHeight = 0;
+	screenWidth = 0;
+}
---
+++ src/GlobalSettings.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "SDL.h"
+#include "Discovery.h"
+using namespace std;
+class GlobalSettingsProfile {
+	ServerHost* host;
+	int screenHeight;
+	int screenWidth;
+	GlobalSettingsProfile();
+};
